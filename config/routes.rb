b3ec30f9bbc82745811ab46643bb956e553c44de
viktor <viktorkarlsson125@gmail.com>
2020-12-10 13:40:24 +0100

    witen request spec for login. added name to factory bot. modify devise_token_auth to api/v2/auth. added namespece api and v2. adding setting in devise_token_auth initializres

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "Admin", at: "admin_auth"
+  mount_devise_token_auth_for "Admin", at: "api/v2/auth", skip: [:omniauth_callbacks]
+
+  namespace :api do
+    namespace :v2 do
+    end
+  end
 end
--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :admin do
     email { "admin@example.com" }
+    name { "Bob" }
     password { "password" }
     password_confirmation { "password" }
   end
