7d42bd7d7954508e0e916b3317823b8f9813e862
viktor <viktorkarlsson125@gmail.com>
2020-12-10 18:05:48 +0100

    adding enum with roles visitor, registered_user and journalist

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,18 @@
 
 class User < ActiveRecord::Base
   extend Devise::Models
+
+  after_initialize :set_default_role, if: :new_record?
+
+  enum role: [:visitor, :registered_user, :journalist]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  private 
+
+  def set_default_role
+    self.role ||= :visitor
+  end
 end
