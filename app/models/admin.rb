d4d3f330ee63345bdf5c33c7c10f91d990f54427
Kyungin Na <nakyungin93@gmail.com>
2020-12-10 20:16:39 +0100

    Deleting admins table from db and deleting test files

--- app/models/admin.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Admin < ActiveRecord::Base
-  extend Devise::Models
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  include DeviseTokenAuth::Concerns::User
-end
--- spec/factories/admins.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :admin do
-    email { "admin@example.com" }
-    name { "Bob" }
-    password { "password" }
-    password_confirmation { "password" }
-  end
-end
